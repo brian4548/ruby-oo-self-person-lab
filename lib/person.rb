e6fc9e4568907cd55016aa0d113595c59053e22f
brian4548 <brianr4548@gmail.com>
2020-11-18 15:43:27 -0600

    changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # your code goes here
+require 'pry'
+
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end 
+
+end
+
+person = Person.new("Brian")
+
